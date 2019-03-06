1928a851705113aa2cb57dd8b731c976f54ea6f7
shannonjohnstone <shannon@shannonjohnstone.com.au>
2019-03-07 09:27:36 +1100

    setup basic routing

--- cms-ui/src/App.js
+++ cms-ui/src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Home from './Home'
 import DummyQuery from './DummyQuery'
 
 import './App.css'
@@ -16,7 +19,12 @@ class App extends Component {
         <div className="App">
           <header className="App-header" />
           <p>This is the app</p>
-          <DummyQuery />
+          <Router>
+            <Fragment>
+              <Route path="/" exact component={Home} />
+              <Route path="/faqs" exact component={DummyQuery} />
+            </Fragment>
+          </Router>
         </div>
       </ApolloProvider>
     )
