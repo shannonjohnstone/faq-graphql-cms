b93e42ace653738513228e93d2666461de357512
shannonjohnstone <shannon@shannonjohnstone.com.au>
2019-03-06 15:09:28 +1100

    initial service project setup setup eslint/prettier/babel

---
+++ cms-service/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    'key-spacing': ['error', { 'afterColon': true }],
+    // 'semi': ['error', 'never'],
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+    'max-len': ['error', 80, {
+      "ignoreComments": true,
+      "ignoreStrings": true
+    }],
+    "node/no-unsupported-features/es-syntax": ["error", {
+      "version": ">=6.0.0",
+      "ignores": ["modules", "trailingCommasInFunctions"]
+    }]
+  }
+}
